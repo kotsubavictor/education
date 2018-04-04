45108e5193600ed9abb7a05382fe9dc5d2d877c5
kotsubavictor <kotsubavictor@gmail.com>
2018-04-04 21:33:26 +0300

    tutorial

---
+++ java/hiberante/hibernate_connection_pool/src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.hibernate.Session;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maven + Hibernate + MySQL");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        Stock stock = new Stock();
+
+        stock.setStockCode("4715");
+        stock.setStockName("GENM");
+
+        session.save(stock);
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+        System.out.println("exit");
+    }
+}
---
+++ java/hiberante/hibernate_connection_pool/src/main/java/Stock.java
@@ -0,0 +1,53 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "stock", catalog = "hibernate", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "STOCK_NAME"),
+        @UniqueConstraint(columnNames = "STOCK_CODE") })
+public class Stock implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    private Integer stockId;
+    private String stockCode;
+    private String stockName;
+
+    public Stock() {
+    }
+
+    public Stock(String stockCode, String stockName) {
+        this.stockCode = stockCode;
+        this.stockName = stockName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "STOCK_ID", unique = true, nullable = false)
+    public Integer getStockId() {
+        return this.stockId;
+    }
+
+    public void setStockId(Integer stockId) {
+        this.stockId = stockId;
+    }
+
+    @Column(name = "STOCK_CODE", unique = true, nullable = false, length = 10)
+    public String getStockCode() {
+        return this.stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    @Column(name = "STOCK_NAME", unique = true, nullable = false, length = 20)
+    public String getStockName() {
+        return this.stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+}
