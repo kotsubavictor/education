5897704080fbc3c60ba071019fbf007bb1d0d4b4
kotsubavictor <kotsubavictor@gmail.com>
2018-04-06 19:24:34 +0300

    tutorial

---
+++ java/hiberante/hibernate_many_to_many_association/src/main/java/Category.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Category {
+    private Integer categoryId;
+    private String name;
+    private String desc;
+    private Set<Stock> stocks = new HashSet<>(0);
+
+    public Category(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Set<Stock> getStocks() {
+        return stocks;
+    }
+
+    public void setStocks(Set<Stock> stocks) {
+        this.stocks = stocks;
+    }
+}
